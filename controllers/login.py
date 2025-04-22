57d39bb4a16ff85cbf1556a5de8a6fcbb1a426ec
Vikas H B <vikasbhapri@gmail.com>
2025-04-22 20:45:43 +0530

    Minor modifications

--- controllers/login.py
+++ controllers/login.py
@@ -7,7 +7,7 @@ from schemas import login as login_schema
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from fastapi.security import OAuth2PasswordBearer
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 import os
 from dotenv import load_dotenv
 
@@ -24,7 +24,7 @@ EXPIRY_DAYS = int(os.getenv("TOKEN_EXPIRY_DAYS"))
 
 def create_access_token(data: dict, expiry_delta: timedelta = None):
     to_encode = data.copy()
-    expire = datetime.now() + expiry_delta
+    expire = datetime.now(timezone.utc) + expiry_delta
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, JWT_SECRET, algorithm=ALGORITHM)
     return encoded_jwt
@@ -50,7 +50,7 @@ def login(request: login_schema.Login, db: Session, response: Response):
     db.commit()
     db.refresh(refresh_token_obj)
 
-    response.set_cookie(key="refresh_token", value=refresh_token, httponly=True, max_age=60*60*24*30, expires=60*60*24*30, samesite="strict", secure=True)
+    response.set_cookie(key="refresh_token", value=refresh_token, httponly=True, max_age=60*60*24*30, expires=60*60*24*30, samesite="strict", secure=False)
 
     return {"access_token": access_token, "token_type": "bearer"}
 
@@ -68,7 +68,7 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
     except JWTError:
         raise credentials_exception
     
-def refresh_token(token: str = Cookie(None), db: Session = Depends(get_db)):
+def refresh_token(token: str = Cookie(None), response: Response = None, db: Session = Depends(get_db)):
     if not token:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
     
@@ -99,6 +99,8 @@ def refresh_token(token: str = Cookie(None), db: Session = Depends(get_db)):
 
     # Commit the changes to revoke the old refresh token
     db.commit()
+
+    response.set_cookie(key="access_token", value=new_access_token, httponly=True, max_age=60*30, expires=60*30, samesite="strict", secure=False)
 
     return {"access_token": new_access_token, "token_type": "bearer"}
 
